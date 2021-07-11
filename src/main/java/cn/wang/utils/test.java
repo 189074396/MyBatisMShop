06ad6325d855ef6cd389031a50d1474da3ea7d31
wang <1185349505@qq.com>
2021-07-11 19:05:09 +0800

    pull commit

---
+++ src/main/java/cn/wang/utils/test.java
@@ -0,0 +1,13 @@
+package cn.wang.utils;
+
+/**
+ * @program: MyBatisMShop
+ * @description:
+ * @Package: cn.wang.utils
+ * @ClassName: test
+ * @author: wang
+ * @create: 2021/07/11 19:02
+ * @Version: 1.0
+ */
+public class test {
+}
